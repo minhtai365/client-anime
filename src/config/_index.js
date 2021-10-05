737eccab6c4cef5d29ae1fd476b61032983351c5
Minh Tài <tranminhtai365@gmail.com>
2021-10-05 21:18:45 +0700

    url api

--- src/config/_index.js
+++ src/config/_index.js
@@ -1,7 +1,10 @@
 // export const API_URL = "https://dien-lanh.herokuapp.com";
 // export const API_URL = "http://192.168.1.11:4000";
 // export const API_URL = "http://192.168.1.112:8080/";
-export const API_URL = "http://localhost:4000/api/v1";
-export const API_URL_SOURCE = "http://localhost:4000/";
+// export const API_URL = "http://localhost:4000/api/v1";
+// export const API_URL_SOURCE = "http://localhost:4000/";
+export const API_URL = "https://server-anime.vercel.app/api/v1";
+export const API_URL_SOURCE = "https://server-anime.vercel.app/";
+
 // export const API_URL = "https://dienlanhz.vercel.app";
 // export const API_URL = "";
