6054655ef071f2d72f1ea8f08b7d62bf5d1f2ffe
Minh Tài <tranminhtai365@gmail.com>
2021-10-06 12:40:57 +0700

    config

--- src/config/_index.js
+++ src/config/_index.js
@@ -3,8 +3,11 @@
 // export const API_URL = "http://192.168.1.112:8080/";
 // export const API_URL = "http://localhost:4000/api/v1";
 // export const API_URL_SOURCE = "http://localhost:4000/";
-export const API_URL = "https://server-anime.vercel.app/api/v1";
-export const API_URL_SOURCE = "https://server-anime.vercel.app/";
+// export const API_URL = "https://server-anime.vercel.app/api/v1";
+// export const API_URL_SOURCE = "https://server-anime.vercel.app/";
+export const API_URL = "https://animetv-server.vercel.app/api/v1";
+export const API_URL_SOURCE = "https://animetv-server.vercel.app/";
+
 
 // export const API_URL = "https://dienlanhz.vercel.app";
 // export const API_URL = "";
